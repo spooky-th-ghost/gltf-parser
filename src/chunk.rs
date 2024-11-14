3d9f7453e28a6fef0ac2594e573a3a20d595d843
spooky-th-ghost <colin@wildheartinteractive.com>
2024-11-14 11:57:03 -0500

    chunk parsing possibly done, need to run it recursively

--- src/chunk.rs
+++ src/chunk.rs
@@ -6,6 +6,32 @@ pub struct Chunk {
     pub data: Vec<u8>,
 }
 
+impl Chunk {
+    pub fn from_slice(mut starting_index: usize, slice: &[u8]) -> Result<(Chunk, usize)> {
+        let mut ending_index: usize = starting_index + 4;
+        let length = slice_to_u32(&slice[starting_index..ending_index])?;
+        starting_index += 4;
+        ending_index = starting_index + 4;
+
+        let type_bytes = <[u8; 4]>::try_from(&slice[starting_index..ending_index])?;
+        let chunk_type: ChunkType = ChunkType::try_from(type_bytes)?;
+
+        starting_index += 4;
+        ending_index = starting_index + length as usize;
+
+        let data = &slice[starting_index..ending_index].to_vec();
+
+        Ok((
+            Chunk {
+                length,
+                chunk_type,
+                data: data.to_owned(),
+            },
+            ending_index,
+        ))
+    }
+}
+
 pub enum ChunkType {
     Json,
     Binary,
@@ -20,6 +46,8 @@ impl std::fmt::Display for ChunkError {
         write!(f, "Chunk Type cannot be parsed")
     }
 }
+
+impl std::error::Error for ChunkError {}
 
 impl TryFrom<[u8; 4]> for ChunkType {
     type Error = ChunkError;
